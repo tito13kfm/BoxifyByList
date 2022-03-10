7f7d80f9069abe4db38ccb5611149244c00062f9
Tito <tito13kfm@gmail.com>
2022-03-10 16:16:13 -0500

    Added comments

--- testList/Program.cs
+++ testList/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
 
+//variable declartion
 bool done = false;
 int length=0;
 char alignment;
 string longestString = "";
 string boxifiedText="";
 string topLeft="", topRight="", bottomLeft="", bottomRight="", vertical="";
+
+//Needed to assign a value to horizontal beforehand otherwise everything breaks
 char horizontal= '▄';
+
+// Setup my list yourText
 List<string> yourText = new List<string>();
 
+//call the functions
 BorderSelect();
 GetAlignment();
 GetText();
@@ -19,7 +25,8 @@ void GetText()
     Console.WriteLine("Type in the text you want me to boxify");
     Console.WriteLine("Type \"end\" to end");
 
-    while (!done)
+    //Continue to get additional lines and store them to List yourText until user enters "end"
+	while (!done)
     {
         string input = Console.ReadLine();
         if (input != "end")
@@ -35,17 +42,21 @@ void GetText()
 
 int FindLongest()
 {
-    foreach (string s in yourText)
+    //Find longest string in List yourText
+	foreach (string s in yourText)
     {
         //mmmm ternary operator
         longestString = s.Length > longestString.Length ? s : longestString;
     }
+	//return length of longest string
     return length = longestString.Length;
 }
 
 void BorderSelect()
 {
-    Console.WriteLine("Please enter the the border style number you want.\n");
+	//stolen wholesale from Mahmut Fesli
+	//Asks for border type, then sets variables for fancy boxes
+	Console.WriteLine("Please enter the the border style number you want.\n");
     Console.WriteLine("╔═══╗	┌───┐	▀▀▀▀▀	░░░░░	▓▓▓▓▓	♥♥♥♥♥	♦♦♦♦♦");
     Console.WriteLine("║ 1 ║	│ 2 │	█ 3 █	░ 4 ░	▓ 5 ▓	♥ 6 ♥	♦ 7 ♦");
     Console.WriteLine("╚═══╝	└───┘	▄▄▄▄▄	░░░░░	▓▓▓▓▓	♥♥♥♥♥	♦♦♦♦♦\n");
@@ -124,6 +135,7 @@ void BorderSelect()
 
 char GetAlignment()
 {
+	//almost certainly overkill to put this in a function... but why not
 	Console.WriteLine("Choose how you want the text aligned");
 	Console.WriteLine("[L]eft, [R]ight, or [C]enter");
 	alignment = Console.ReadLine().ToUpper().FirstOrDefault();
@@ -132,12 +144,15 @@ char GetAlignment()
 
 string Boxify(List<string> yourText, int length, char alignment)
 {
+	//setup top border
 	string border = new string(horizontal, length + 4);
 	boxifiedText = topLeft + border + topRight;
 
 	switch (alignment)
 	{
+		//All the stuff for Left align
 		case 'L':
+			//build the text between top and bottom borders.
 			foreach (string s in yourText)
 			{
 				int padLength = border.Length - s.Length -1;
@@ -145,23 +160,32 @@ string Boxify(List<string> yourText, int length, char alignment)
 				boxifiedText = boxifiedText + "\n" + vertical + " " + s + padding + vertical;
 			}
 			break;
+		
+		//Center align
 		case 'C':
+			//formatting broke on odd length inputs, so hacked it to go up to next integer
 			length = length % 2 == 0 ? length : length + 1;
 			border = new string(horizontal, length + 4);
+
+			//remaking the top border to fit with new length
 			boxifiedText = topLeft + border + topRight;
+			
+			//Building the gooey inards of the box
 			foreach (string s in yourText)
 			{
 				int padLength = (length - s.Length + 4) / 2;
 				string padding = new string(' ', padLength);
 
 				//This is the line of code that almost broke me.
+				//If the length of the padding is even, then keep it.  If it's odd, make it even
 				padLength = s.Length % 2 == 0 ? padLength: padLength + 1;
 				string paddingRight = new string(' ', padLength);
 
 				boxifiedText = boxifiedText + "\n" + vertical + padding + s + paddingRight + vertical;
 			}
-
 			break;
+
+		//Right align
 		case 'R':
 			foreach (string s in yourText)
 			{
@@ -170,10 +194,13 @@ string Boxify(List<string> yourText, int length, char alignment)
 				boxifiedText = boxifiedText + "\n" + vertical + padding + s + "  " + vertical;
 			}
 			break;
+
+		//Should probably make the default do something...
 		default:
 			break;
 	}
 
+	//stick on the bottom border
 	boxifiedText = boxifiedText + "\n" + bottomLeft + border + bottomRight;
 	return boxifiedText;
 }
